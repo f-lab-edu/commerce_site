a2109d32e003733bc585db1287a6c1cf68804b14
suhaoh <suha.oh91@gmail.com>
2024-09-09 20:39:24 +0900

    [25] 코드리뷰 반영
    - 페이지를 1로 받도록하고 내부에서 0으로 처리하도록 수정
    - 불필요한 주석 삭제

--- src/main/java/org/example/commerce_site/representation/address/AddressController.java
+++ src/main/java/org/example/commerce_site/representation/address/AddressController.java
@@ -1,11 +1,9 @@
 package org.example.commerce_site.representation.address;
 
 import org.example.commerce_site.application.address.AddressFacade;
-import org.example.commerce_site.application.address.dto.AddressResponseDto;
 import org.example.commerce_site.common.response.ApiSuccessResponse;
 import org.example.commerce_site.representation.address.request.AddressRequest;
 import org.example.commerce_site.representation.address.response.AddressResponse;
-import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -47,7 +45,7 @@ public class AddressController {
 		@PathVariable(name = "user_id") Long userId, //TODO 추후 토큰에서 가져오도록 함
 		@RequestParam(value = "page", defaultValue = "1") int page,
 		@RequestParam(value = "size", defaultValue = "10") int size) {
-		return ApiSuccessResponse.success(AddressResponse.Get.of(addressFacade.getList(userId, page, size)));
+		return ApiSuccessResponse.success(AddressResponse.Get.of(addressFacade.getList(userId, page - 1, size)));
 	}
 
 	@PatchMapping("/{user_id}/{address_id}")
