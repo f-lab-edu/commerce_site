18f680890777975950e09a847d00037c4059da27
suhaoh <suha.oh91@gmail.com>
2024-09-27 23:40:10 +0900

    [32] reformat code

--- src/main/java/org/example/commerce_site/application/order/dto/OrderDetailResponseDto.java
+++ src/main/java/org/example/commerce_site/application/order/dto/OrderDetailResponseDto.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.example.commerce_site.domain.Order;
 import org.example.commerce_site.domain.OrderDetail;
 
 import lombok.Builder;
@@ -17,15 +18,29 @@ public class OrderDetailResponseDto {
 		private Long id;
 		private Long productId;
 		private Long quantity;
+		private Long orderId;
 		private BigDecimal unitPrice;
 
-		public static OrderDetailResponseDto.Get toDto(OrderDetail orderDetail) {
+
+		public static Get toDto(OrderDetail orderDetail) {
 			return Get.builder()
-				.id(orderDetail.getId())
-				.productId(orderDetail.getProductId())
-				.unitPrice(orderDetail.getUnitPrice())
-				.quantity(orderDetail.getQuantity())
 				.createdAt(orderDetail.getCreatedAt())
+				.id(orderDetail.getId())
+				.orderId(orderDetail.getOrder().getId())
+				.productId(orderDetail.getProductId())
+				.quantity(orderDetail.getQuantity())
+				.unitPrice(orderDetail.getUnitPrice())
+				.build();
+		}
+
+		public static OrderDetail toEntity(Get orderDetailItem, Order order) {
+			return OrderDetail.builder()
+				.id(orderDetailItem.id)
+				.order(order)
+				.productId(orderDetailItem.productId)
+				.unitPrice(orderDetailItem.unitPrice)
+				.quantity(orderDetailItem.quantity)
+				.createdAt(orderDetailItem.createdAt)
 				.build();
 		}
 
